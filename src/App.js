1795692d7a511f5c4a3f66416a0e130094af010d
Franklin Bwana <bwanafranklin@gmail.com>
2023-08-30 23:44:37 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ const data = [
+  {
+    Question: "Question1",
+    Content: "Good morning. This is District Court 32-2-37 in the case of the Commonwealth v. Kevin Watson. This is CR 95 of 2023. The defendant is present but represented by counsel. Will counsel for the defense, please identify yourself for the record.",
+  },
+  {
+    Question: "Question1",
+    Content: "Good morning. This is District Court 32-2-37 in the case of the Commonwealth v. Kevin Watson. This is CR 95 of 2023. The defendant is present but represented by counsel. Will counsel for the defense, please identify yourself for the record.",
+  },
+  {
+    Question: "Question1",
+    Content: "Good morning. This is District Court 32-2-37 in the case of the Commonwealth v. Kevin Watson. This is CR 95 of 2023. The defendant is present but represented by counsel. Will counsel for the defense, please identify yourself for the record."
+  },
+  
+  {
+    Question: "Question1",
+    Content: "Good morning. This is District Court 32-2-37 in the case of the Commonwealth v. Kevin Watson. This is CR 95 of 2023. The defendant is present but represented by counsel. Will counsel for the defense, please identify yourself for the record."
+  },
+  
+ ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="accordion">
+       {data.map((item, i)=>(
+        <div className="item">
+         <div className="title">
+          <h2>{item.Question}</h2>
+          <span>+</span>
+         </div>
+         <div className="content">{item.Content}</div>
+        </div>
+       ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
